2bc9d3a3d00c2349eb32608b4c34f6aabfeb19c9
bradzhang1128@gmail.com <deadbred3@gmail.com>
2017-05-05 16:59:08 -0400

    error conditions

--- app/controllers/api/races_controller.rb
+++ app/controllers/api/races_controller.rb
@@ -2,6 +2,10 @@ module Api
 	class RacesController < ApplicationController
 		protect_from_forgery with: :null_session
 
+		rescue_from Mongoid::Errors::DocumentNotFound do |exception|
+			render plain: "woops: cannot find race[#{params[:id]}]", status: :not_found
+		end
+
 		def index
 			if !request.accept || request.accept == "*/*"
 				render plain: "/api/races, offset=[#{params[:offset]}], limit=[#{params[:limit]}]" 
