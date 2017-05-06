9915c7490bdf1ec08fdb773cefe383b719c0beb5
bradzhang1128@gmail.com <deadbred3@gmail.com>
2017-05-05 20:32:13 -0400

    representations

--- app/controllers/api/races_controller.rb
+++ app/controllers/api/races_controller.rb
@@ -3,8 +3,21 @@ module Api
 		protect_from_forgery with: :null_session
 
 		rescue_from Mongoid::Errors::DocumentNotFound do |exception|
-			render plain: "woops: cannot find race[#{params[:id]}]", status: :not_found
+			@msg = "woops: cannot find race[#{params[:id]}]"
+			if !request.accept || request.accept == "*/*"
+				render plain: @msg, status: :not_found
+			else
+				respond_to do |format|
+					format.json { render "api/error_msg", status: :not_found, content_type: "#{request.accept}"}
+					format.xml  { render "api/error_msg", status: :not_found, content_type: "#{request.accept}"}
+				end
+			end
 		end
+
+		rescue_from ActionView::MissingTemplate do |exception|
+			render plain: "woops: we do not support that content-type[#{request.accept}]", :status => 415
+        end
+
 
 		def index
 			if !request.accept || request.accept == "*/*"
@@ -16,12 +29,17 @@ module Api
 
 		def show
 			if !request.accept || request.accept == "*/*"
-				render plain: "/api/races/#{params[:id]}" 
-			else
-			    race = Race.find(params[:id])
-			    render json: race
-			end 
-		end
+              #render plain: "woops: cannot find race[#{params[:id]}]", status: :not_found
+               render :status=>:not_found, 
+                      :template=>"api/error_msg", 
+                      :locals=>{ :msg=>"woops: cannot find race[#{params[:id]}]"}
+            else
+              #real implementation ...
+              @race = Race.find(params[:id])
+               #render json: race, status: :ok
+              render @race, content_type: "#{request.accept}"
+            end
+        end 
 
 		def create
 			if !request.accept || request.accept == "*/*"
